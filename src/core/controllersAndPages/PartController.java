b12f789f124e861b0c0cf64897c9c43fb83bc252
eric mullally <ericmullally@yahoo.com>
2021-06-21 15:35:27 -0500

    added check Fields function to parts controller.

--- src/core/controllersAndPages/PartController.java
+++ src/core/controllersAndPages/PartController.java
@@ -2,16 +2,20 @@ package core.controllersAndPages;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.util.Pair;
+import javafx.scene.layout.GridPane;
+import java.text.MessageFormat;
 
-import java.util.List;
+
 
 /**
  * controller for the parts page.
  */
 public class PartController {
+    @FXML
+    private GridPane add_part_grid;
 
     @FXML
     private TextField add_part_name_field,add_part_inv_field,add_part_price_field,
@@ -33,13 +37,19 @@ public class PartController {
      * Checks that all required fields have been filled.
      */
     public boolean checkFields(){
-        String[] fields = {add_part_name_field.getText(),add_part_machine_field.getText(), add_part_inv_field.getText(),
-                add_part_price_field.getText(),add_part_max_field.getText(), add_part_min_field.getText() };
-        int blankFieldposition = -1;
+        TextField[] fields = {add_part_name_field, add_part_machine_field, add_part_inv_field,
+                add_part_price_field,add_part_max_field, add_part_min_field };
 
         for(int i = 0; i < fields.length; i++){
-            if(fields[i].isBlank()){
-                blankFieldposition = i;
+            if(fields[i].getText().isEmpty()){
+
+                String[] fieldNameList = fields[i].getId().split("_");
+                String fieldName = "Part " + fieldNameList[fieldNameList.length -2];
+                String information = MessageFormat.format("The following Field is empty: {0}", fieldName);
+                Alert errorMsg = new Alert(Alert.AlertType.ERROR);
+                errorMsg.setHeaderText("Empty Field");
+                errorMsg.setContentText(information);
+                errorMsg.show();
                 return false;
             }
         }
